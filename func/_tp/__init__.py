ab3303f44f80a32a242003898f0dd1a2f16c2849
jeff <pub67073@mzico.com>
2022-06-02 14:56:29 +0800

    feat: 擴充Macro功能

--- func/_tp/__init__.py
+++ func/_tp/__init__.py
@@ -4,7 +4,12 @@
 from func._tp._klp._ma._stone import (stone_pp003, stone_pp004, stone_pp005,
                                       stone_pp006, stone_pp007, stone_pp008,
                                       stone_pp009)
+from func._tp._ma._jack import (jack_ma_order_up, jack_ma_order_thick, jack_ma_order_down,
+                                jack_ma_through_price_down, jack_ma_through_price_up,
+                                jack_ma_through_ma_down_thrend, jack_ma_through_ma_up_thrend)
 
 __all__ = []
 __all__ += ['stone_pp003', 'stone_pp004', 'stone_pp005', 'stone_pp006',
             'stone_pp007', 'stone_pp008', 'stone_pp009']
+__all__ += ['jack_ma_order_up', 'jack_ma_order_thick', 'jack_ma_order_down', 'jack_ma_through_price_down',
+            'jack_ma_through_price_up', 'jack_ma_through_ma_down_thrend', 'jack_ma_through_ma_up_thrend']
