fbf29723a11733845990ac201c1aa88e3f9229b4
DouTze <k40291113@gmail.com>
2022-09-21 15:40:08 +0800

    fix: 修復上板流程問題
    避開重新安裝 conda 所造成的檔案權限鎖死問題

--- install.bat
+++ install.bat
@@ -1,4 +1,2 @@
-md "../env"
-call conda remove -p "../env/mimosa" --force-remove -y --all
-md "../env/mimosa"
-call conda env create --file "./requirements.yml" --prefix "../env/mimosa"
+call activate ../env/mimosa
+pipenv install Pipfile
